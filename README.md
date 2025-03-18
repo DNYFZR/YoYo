aaa26bdfbac49e5ec9aa71df54ab15456e47f5bd
Danny <76977155+DNYFZR@users.noreply.github.com>
2025-03-18 12:20:34 +0000

    update-to-prod (#15)
    * added get method
    
    * updated tests & format
    
    * updated docs
    
    * updated build tools

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 <h1 align="center">🪀 YoYo 🪀</h1>
 
-<h3 align="center"><b>For Your YoYo Data Flow...</b></h3>
-
 [![YoYoCICD](https://github.com/DNYFZR/YoYo/actions/workflows/build.yaml/badge.svg)](https://github.com/DNYFZR/YoYo/actions/workflows/build.yaml)
 
 **🪀YoYo Provides serverless caching services with backup functionality:**
@@ -10,6 +8,7 @@
 - Wrapped inside a light weight & powerful API.
 - Users have access to underlying DuckDB functionaliy via the ```YoYo().connect()``` method.
 - Users also have access to the power of Rust via Polars, along side the main caching mechanisms it's involved in.
+- The trick deck: [API Reference](/docs/API.md)
 
 <br>
 
@@ -17,12 +16,8 @@
 
 - Polars (no mandatory dependencies)
 - DuckDB (no mandatory dependencies)
-- PyArrow (requires NumPy)
+- PyArrow (no mandatory dependencies)
 
 <br>
-
-**🪀 YoYo's Trick Deck :**
-
-In time, more detail will be added... For now here's what we've got : [API Reference](/docs/API.md)
 
 ---
--- tests/test_yoyo.py
+++ tests/test_yoyo.py
@@ -1,136 +1,132 @@
 # YoYo Cache Testing Module
-
 import os, pytest, duckdb, polars as pl
 from yoyo import YoYo
 
-test_case = {
-  "table": "test_table",
-  "source": "test_df",
-  "backup": f"/tmp/test_backup/",
-  "db" : f"test_cache",
-  "erase_db": "eraser",
-  "erase_table": "tmp_table",
-}
-
 os.makedirs(f"{os.getcwd()}/tmp", exist_ok=True)
 
-test_df = pl.DataFrame({
-  "A": [1,2,3],
-  "B": ["A", "B", "C"],
-  "C": [2,4,6],
-  "D": [0.1, 0.2, 0.3],
-  })
+test_case = {
+    "table": "test_table",
+    "source": pl.DataFrame({
+        "A": [1,2,3],
+        "B": ["A", "B", "C"],
+        "C": [2,4,6],
+        "D": [0.1, 0.2, 0.3],
+        }),
+    "backup": f"/tmp/test_backup/",
+    "db" : f"test_cache",
+    "erase_db": "eraser",
+    "erase_table": "tmp_table",
+}
 
 @pytest.mark.parametrize("test_case", [test_case, ])
 class TestCache:
-  """Test the functionality of the Cache class"""
-  def test_init(self, test_case):
-    """Test initialisation of the Cache class"""
-    obj  = YoYo(db=test_case["db"]).__dir__()
+    """Test the functionality of the Cache class"""
+    def test_init(self, test_case):
+        """Test initialisation of the Cache class"""
+        obj  = YoYo(db=test_case["db"]).__dir__()
 
-    if "cache_dir" not in obj:
-      raise AssertionError(f"Cache initialisation error : 'cache_dir' not in object")
+        if "cache_dir" not in obj:
+            raise AssertionError(f"Cache initialisation error : 'cache_dir' not in object")
+        
+        if "db" not in obj:
+            raise AssertionError(f"Cache initialisation error : 'db' not in object")
+        
+        if "schema" not in obj:
+            raise AssertionError(f"Cache initialisation error : 'schema' not in object")
+        
+
+    def test_connect(self, test_case):
+        """Test the connect method"""
+        conn = YoYo(db=test_case["db"]).connect()
+
+        if not isinstance(conn, duckdb.DuckDBPyConnection):
+            del conn
+            raise AssertionError("Cache connection test failed")
+          
+        conn.close()
+        
+
+    def test_info(self, test_case):
+        """Test the info method"""
+        cache = YoYo(db=test_case["db"]).info()
+
+        if not isinstance(cache, list):
+            raise AssertionError("Cache info data structure test failed")
+
+
+    def test_list_all_tables(self, test_case):
+        """Test the list tables (no schema info) method"""
+        cache = YoYo(db=test_case["db"]).list_all_tables()
+
+        if not isinstance(cache, list):
+            raise AssertionError("Cache list tables data structure test failed")
     
-    if "db" not in obj:
-      raise AssertionError(f"Cache initialisation error : 'db' not in object")
-    
-    if "schema" not in obj:
-      raise AssertionError(f"Cache initialisation error : 'schema' not in object")
-    
 
-  def test_connect(self, test_case):
-    """Test the connect method"""
-    conn = YoYo(db=test_case["db"]).connect()
+    def test_list_schema_tables(self, test_case):
+        """Test the list tables (with schema info) method"""
+        cache = YoYo(db=test_case["db"]).list_schema_tables()
 
-    if not isinstance(conn, duckdb.DuckDBPyConnection):
-      del conn
-      raise AssertionError("Cache connection test failed")
-    
-    conn.close()
+        if not isinstance(cache, list):
+            raise AssertionError("Cache list schema tables data structure test failed")
     
 
-  def test_info(self, test_case):
-    """Test the info method"""
-    cache = YoYo(db=test_case["db"]).info()
+    def test_update(self, test_case):
+        """Test the table update method creates a table using in memory data"""
+        try:
+            cache = YoYo(db=test_case["db"])
+            cache.connect()
+            cache.update(table=test_case["table"], source=test_case["source"])
 
-    if not isinstance(cache, list):
-      raise AssertionError("Cache info data structure test failed")
-
-
-  def test_list_all_tables(self, test_case):
-    """Test the list tables (no schema info) method"""
-    cache = YoYo(db=test_case["db"]).list_all_tables()
-
-    if not isinstance(cache, list):
-      raise AssertionError("Cache list tables data structure test failed")
-  
-
-  def test_list_schema_tables(self, test_case):
-    """Test the list tables (with schema info) method"""
-    cache = YoYo(db=test_case["db"]).list_schema_tables()
-
-    if not isinstance(cache, list):
-      raise AssertionError("Cache list schema tables data structure test failed")
-  
-
-  def test_update(self, test_case):
-    """Test the table update method creates a table using in memory data"""
-    try:
-      cache = YoYo(db=test_case["db"])
-      cache.connect()
-      cache.update(table=test_case["table"], source=test_case["source"])
-
-    except Exception as e:
-      raise AssertionError(f"Test update method failed with error : {e}")
+        except Exception as e:
+           raise AssertionError(f"Test update method failed with error : {e}")
 
 
-  def test_check(self, test_case):
-    """Test the cache table checking method"""
-    try:
-      YoYo(db=test_case["db"]).check(test_case["table"])
+    def test_check(self, test_case):
+        """Test the cache table checking method"""
+        try:
+           YoYo(db=test_case["db"]).check(test_case["table"])
 
-    except Exception as e:
-      raise AssertionError(f"Test table checking method failed with exeption :{e}")
+        except Exception as e:
+            raise AssertionError(f"Test table checking method failed with exeption :{e}")
 
 
-  def test_backup(self, test_case):
-    """Test the cache backup method creates a backup"""
-    cache = YoYo(db=test_case["db"])
-    cache.update(test_case["table"], test_case["source"])
-    cache.backup(cache_dir=f"{os.getcwd()}/{test_case["backup"]}")
-    _, _, backup_files = list(os.walk(f"{os.getcwd()}/{test_case["backup"]}"))[0]
+    def test_backup(self, test_case):
+        """Test the cache backup method creates a backup"""
+        cache = YoYo(db=test_case["db"])
+        cache.update(test_case["table"], test_case["source"])
+        cache.backup(cache_dir=f"{os.getcwd()}/{test_case["backup"]}")
+        _, _, backup_files = list(os.walk(f"{os.getcwd()}/{test_case["backup"]}"))[0]
 
-    if len(backup_files) == 0:
-      raise AssertionError("Test backup method failed : no backup was detected")
+        if len(backup_files) == 0:
+            raise AssertionError("Test backup method failed : no backup was detected")
+        
       
-    
-  def test_erase(self, test_case):
-    """Test the cache erase method removes the cache file"""
-    cache = YoYo(db=test_case["erase_db"])
-    cache.erase()
-    
-    if os.path.exists(test_case["erase_db"]):
-      raise AssertionError("Test erase method failed : cache file detected")
-    
+    def test_erase(self, test_case):
+        """Test the cache erase method removes the cache file"""
+        cache = YoYo(db=test_case["erase_db"])
+        cache.erase()
+        
+        if os.path.exists(test_case["erase_db"]):
+            raise AssertionError("Test erase method failed : cache file detected")
+      
 
-  def test_erase_backup(self, test_case):
-    """Test the cache erase backup method removes backup files"""
-    cache = YoYo(db=test_case["db"])
-    cache.backup()
-    _, _, backup_files = list(os.walk(cache.cache_dir))[0]
-    cache.erase_backup()
+    def test_erase_backup(self, test_case):
+        """Test the cache erase backup method removes backup files"""
+        cache = YoYo(db=test_case["db"])
+        cache.backup()
+        _, _, backup_files = list(os.walk(cache.cache_dir))[0]
+        cache.erase_backup()
 
-    if any([os.path.exists(i) for i in backup_files]):
-        raise AssertionError("Test erase backup method failed : backup files detected")
-    
+        if any([os.path.exists(i) for i in backup_files]):
+          raise AssertionError("Test erase backup method failed : backup files detected")
+      
 
-  def test_clear(self, test_case):
-    """Test the cache clear method erases the cache 
-       but leaves the file in place"""
-    cache = YoYo(db=test_case["db"])
-    cache.update(test_case["erase_table"], test_case["source"])
-    cache.clear(table=test_case["erase_table"])
+    def test_clear(self, test_case):
+        """Test the cache clear method erases the cache 
+          but leaves the file in place"""
+        cache = YoYo(db=test_case["db"])
+        cache.update(test_case["erase_table"], test_case["source"])
+        cache.clear(table=test_case["erase_table"])
 
-    if test_case["erase_table"] in cache.list_all_tables():
-      raise AssertionError("Test clear method failed : table detected in cache")
-    
+        if test_case["erase_table"] in cache.list_all_tables():
+           raise AssertionError("Test clear method failed : table detected in cache")
