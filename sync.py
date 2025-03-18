de85b432d7d73999852388ef7f2fcb64a87dd403
Danny <76977155+DNYFZR@users.noreply.github.com>
2025-03-18 12:12:10 +0000

    update build command

--- sync.py
+++ sync.py
@@ -11,7 +11,6 @@ def sync_env(path:str):
         
         build = open(path).read().split("dependencies = [")[1].split("]\n")[0].split(",")
         build = [i.strip().strip('"') for i in build]
-        print(build)
         [subprocess.check_call([sys.executable, '-m', 'pip', 'install', package]) for package in build if len(package) > 0]
                 
         # Install test dependencies
